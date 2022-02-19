a6e8ecd3edd2a3ae0fcc3b2029570224b6bcd678
Nabil Hadi <napilfadl@gmail.com>
2022-02-19 15:44:52 +0300

    code refactor

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   const rpsArray = ["rock", "paper", "scissors"];
   const randNum = Math.floor(Math.random() * 3);
@@ -10,59 +13,77 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
-      return "draw: It is a draw!";
+      return "It is a draw!";
     } else if (computerSelection === "paper") {
-      return "lose: You Lose! Paper beats Rock";
+      computerScore++;
+      return "You Lose! Paper beats Rock";
     } else if (computerSelection === "scissors") {
-      return "win: You Win! Rock beats Scissors";
+      playerScore++;
+      return "You Win! Rock beats Scissors";
     } else {
       return "error: Wrong computer Input";
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      return "win:You Win! Paper beats Rock";
+      playerScore++;
+      return "You Win! Paper beats Rock";
     } else if (computerSelection === "paper") {
-      return "draw:It is a draw!";
+      return "It is a draw!";
     } else if (computerSelection === "scissors") {
-      return "lose:You Lose! Scissors beats Paper";
+      computerScore++;
+      return "You Lose! Scissors beats Paper";
     } else {
-      return "error:Wrong computer Input";
+      return "error: Wrong computer Input";
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      return "lose:You Lose! Rock beats Scissors";
+      computerScore++;
+      return "You Lose! Rock beats Scissors";
     } else if (computerSelection === "paper") {
-      return "win:You Win! Scissors beats Paper";
+      playerScore++;
+      return "You Win! Scissors beats Paper";
     } else if (computerSelection === "scissors") {
-      return "draw:It is a draw!";
+      return "It is a draw!";
     } else {
-      return "error:Wrong computer Input";
+      return "error: Wrong computer Input";
     }
   } else {
     return "error: Wrong player Input";
   }
 }
 
-function game() {
-  let totalGames = 5;
-  let score = 0;
-  for (let i = 0; i < totalGames; i++) {
-    const playerSelection = prompt("rock, paper, scissors. \nEnter your choice: ");
-    const result = playRound(playerSelection, computerPlay());
-    if (result.startsWith("win:")) {
-      score++;
-      console.log(result.substring("win:".length));
-    } else if (result.startsWith("lose:")) {
-      console.log(result.substring("lose:".length));
-    } else if (result.startsWith("draw:")) {
-      console.log(result.substring("draw:".length));
-    } else {
-      console.log(result);
-    }
-  }
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+const resetBtn = document.querySelector("#resetBtn");
+const resultDiv = document.querySelector("#resultBtn");
+const scoreDiv = document.querySelector("#scoreBtn");
 
-  console.log(`You won ${score} ${score > 1 ? "games" : "game"} ` +
-    `out of ${totalGames} ${totalGames > 1 ? "games" : "game"}`);
+function clickHandler(event) {
+  resultDiv.textContent = playRound(event.target.textContent.toLowerCase(), computerPlay());
+  scoreDiv.textContent = `Player: ${playerScore} -- Computer: ${computerScore}`;
+  if (playerScore === 5 || computerScore === 5) {
+    rockBtn.removeEventListener("click", clickHandler);
+    paperBtn.removeEventListener("click", clickHandler);
+    scissorsBtn.removeEventListener("click", clickHandler);
+    resultDiv.textContent = `${playerScore === 5 ? "You" : "Computer"} Won!!`;
+  }
 }
+
+rockBtn.addEventListener("click", clickHandler);
+paperBtn.addEventListener("click", clickHandler);
+scissorsBtn.addEventListener("click", clickHandler);
+
+resetBtn.addEventListener("click", () => {
+  resultDiv.textContent = "";
+  scoreDiv.textContent = "";
+  playerScore = 0;
+  computerScore = 0;
+
+  rockBtn.addEventListener("click", clickHandler);
+  paperBtn.addEventListener("click", clickHandler);
+  scissorsBtn.addEventListener("click", clickHandler);
+});
+
 
 
